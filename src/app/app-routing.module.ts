e7d7c6171e5a6696dd62d0b99e18eaf042f4c479
Maciej Jamer <mjamer@concisesoftware.com>
2020-03-17 15:18:35 +0100

    after 14 video

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
+import { EventsComponent } from './events/events.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/events',
+    pathMatch: 'full'
+  },
+  {
+    path: 'events',
+    component: EventsComponent
+  },
+  {
+    path: 'special',
+    component: SpecialEventsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
