a3282547f68322657146f718bcd88b9fb80df10b
Maciej Jamer <mjamer@concisesoftware.com>
2020-03-18 11:34:41 +0100

    after 26 video

--- src/app/special-events/special-events.component.ts
+++ src/app/special-events/special-events.component.ts
@@ -1,5 +1,7 @@
+import { Router } from '@angular/router';
 import { EventService } from './../services/event.service';
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-special-events',
@@ -9,12 +11,18 @@ import { Component, OnInit } from '@angular/core';
 export class SpecialEventsComponent implements OnInit {
   specialEvents = [];
 
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService, private router: Router) { }
 
   ngOnInit(): void {
     this.eventService.getSpecialEvents().subscribe(
       res => this.specialEvents = res,
-      err => console.log(err)
+      err => {
+        if ( err instanceof HttpErrorResponse ) {
+          if ( err.status === 401 ) {
+            this.router.navigate(['/login']);
+          }
+        }
+      }
     );
   }
 
